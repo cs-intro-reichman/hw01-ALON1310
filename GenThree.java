463e62dcc46e199ef61884104569c95f81e86aa0
AlonBarShimon <alon.barshimon@post.runi.ac.il>
2023-12-25 12:14:41 +0200

    homework

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,29 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+	
+	int a = Integer.parseInt(args[0]);
+	int b = Integer.parseInt(args[1]);
+	int c = Math.min(a,b);	
+	int d = Math.max(a,b); 
+
+
+	int num1 = (int)( (Math.random() * ( d - c) ) + c );
+
+	int num2 = (int)( (Math.random() * ( d - c ) ) + c );
+ 
+	int num3 = (int)( (Math.random() * ( d - c ) ) + c );
+
+
+		int min = (int) (Math.min(num1, (int)Math.min(num2,num3)));
+
+		System.out.println(num1);
+
+		System.out.println(num2);
+
+		System.out.println(num3);
+
+
+	System.out.println("The minimal generated number was " + min);
+
 	}
 }
--- LinearEq.java
+++ LinearEq.java
@@ -1,9 +1,15 @@
-/*
- * Solves linear equations of the form a⋅x + b = c.
- * The program gets a, b, and c as command-line arguments,
- * computes x, and prints the result.
- * Treats the three arguments as well as the computed value as double values
- */
-public class LinearEq {
-	// Put your code here
+public class Coins {
+    public static void main(String[] args) {
+    int a = Integer.parseInt(args[0]) ;
+    int b = Integer.parseInt(args[1]) ;
+    int c = Integer.parseInt(args[2]) ;
+    double x = ((c-b)/a); 
+
+  System.out.println(a + "⋅x " + "+ " + b + " = " + c );
+
+
+    System.out.println("X = " + x) ;
+
+    }
 }
+
