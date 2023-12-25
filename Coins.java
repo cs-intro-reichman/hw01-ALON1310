8ecd5e3ae99e7e4d407b2a3a5cb91fd812b4ddfe
AlonBarShimon <alon.barshimon@post.runi.ac.il>
2023-12-25 12:26:49 +0200

    homework

--- Coins.java
+++ Coins.java
@@ -1,7 +1,6 @@
 public class Coins {
     public static void main(String[] args) {
     int coins =Integer.parseInt (args[0]) ; 
-    System.out.println("java coins : " + coins) ;
     int quarters = coins/25 ;
     int cent =(coins - (quarters*25)) ; 
     System.out.println("Use " + quarters + " quarters and " + cent + " cents");
